b8659fc0d819c08b586f48a1c9ec1654cfb68db3
Qylahrzk <nuraqilahnur@gmail.com>
2025-01-23 09:23:02 +0800

    Fix3 - Account Screen

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:image_picker/image_picker.dart'; // To pick profile image
-import 'dart:io'; // For handling file paths
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -13,29 +13,171 @@ class AccountScreen extends StatefulWidget {
 class _AccountScreenState extends State<AccountScreen> {
   final ImagePicker _picker = ImagePicker();
   String? _profilePicture;
-  String? _name = 'User Name';
-  String? _email = 'user@example.com';
+  String _name = 'User Name';
+  String _email = 'user@example.com';
+  late Box userBox;
 
-  // Load the user's details from the Hive box
-  final userBox = Hive.box('user');
+  @override
+  void initState() {
+    super.initState();
+    // Open Hive box in initState
+    userBox = Hive.box('user');
+    _initializeUserData();
+  }
 
-  // Pick a profile picture
+  void _initializeUserData() {
+    setState(() {
+      _profilePicture = userBox.get('profilePicture');
+      _name = userBox.get('name', defaultValue: 'User Name');
+      _email = userBox.get('email', defaultValue: 'user@example.com');
+    });
+  }
+
   Future<void> _pickProfilePicture() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
     if (image != null) {
       setState(() {
         _profilePicture = image.path;
-        userBox.put('profilePicture', _profilePicture); // Save to Hive
+        userBox.put('profilePicture', _profilePicture);
       });
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _name = userBox.get('name', defaultValue: 'User Name');
-    _email = userBox.get('email', defaultValue: 'user@example.com');
-    _profilePicture = userBox.get('profilePicture');
+  void _editProfileName() {
+    TextEditingController nameController = TextEditingController(text: _name);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Edit Profile Name'),
+          content: TextField(
+            controller: nameController,
+            decoration: const InputDecoration(labelText: 'Name'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (nameController.text.isNotEmpty) {
+                  setState(() {
+                    _name = nameController.text;
+                    userBox.put('name', _name);
+                  });
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSecuritySettings() {
+    Navigator.pushNamed(context, '/security');
+  }
+
+  void _toggleSync(bool value) {
+    // Example: Add functionality for sync toggle
+    setState(() {
+      userBox.put('syncEnabled', value);
+    });
+  }
+
+  void _showSortingOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Sorting Options'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: const Text('Date'),
+              onTap: () {
+                userBox.put('sorting', 'Date');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Amount'),
+              onTap: () {
+                userBox.put('sorting', 'Amount');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showSummaryOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Summary Options'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: const Text('Average'),
+              onTap: () {
+                userBox.put('summary', 'Average');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Total'),
+              onTap: () {
+                userBox.put('summary', 'Total');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showCurrencyOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Select Default Currency'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: const Text('USD (\$)'),
+              onTap: () {
+                userBox.put('currency', 'USD (\$)');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('EUR (€)'),
+              onTap: () {
+                userBox.put('currency', 'EUR (€)');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('GBP (£)'),
+              onTap: () {
+                userBox.put('currency', 'GBP (£)');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -47,7 +189,7 @@ class _AccountScreenState extends State<AccountScreen> {
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              Navigator.pushNamed(context, '/settings'); // Navigate to settings screen
+              Navigator.pushNamed(context, '/settings');
             },
           ),
         ],
@@ -73,68 +215,66 @@ class _AccountScreenState extends State<AccountScreen> {
             ),
             const SizedBox(height: 16),
             Text(
-              _name ?? 'User Name',
+              _name,
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Text(
-              _email ?? 'user@example.com',
+              _email,
               style: const TextStyle(fontSize: 14, color: Colors.grey),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/editProfile'); // Navigate to edit profile
-              },
+              onPressed: _editProfileName,
               child: const Text('Edit Profile'),
             ),
             const SizedBox(height: 20),
             // General Section
-            _buildSectionHeader('General'),
-            _buildListTile('Security', 'Face ID', Icons.security),
-            _buildListTile('iCloud Sync', 'Enabled', Icons.cloud, isSwitch: true),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'General',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 10),
+            _buildListTile('Security', 'Face ID', Icons.security,
+                onTap: _showSecuritySettings),
+            SwitchListTile(
+              title: const Text('Sync'),
+              subtitle: const Text('iCloud Sync'),
+              value: userBox.get('syncEnabled', defaultValue: false),
+              onChanged: _toggleSync,
+            ),
             const SizedBox(height: 20),
-            // My Subscriptions Section
-            _buildSectionHeader('My Subscriptions'),
-            _buildListTile('Sorting', 'Date', Icons.sort),
-            _buildListTile('Summary', 'Average', Icons.summarize),
-            _buildListTile('Default Currency', 'USD (\$)', Icons.monetization_on),
-            const SizedBox(height: 20),
-            // Appearance Section
-            _buildSectionHeader('Appearance'),
-            _buildListTile('App Icon', 'Default', Icons.apps),
-            _buildListTile('Theme', 'Dark', Icons.dark_mode),
+            // Subscriptions Section
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'My Subscriptions',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 10),
+            _buildListTile('Sorting', 'Date', Icons.sort,
+                onTap: _showSortingOptions),
+            _buildListTile('Summary', 'Average', Icons.summarize,
+                onTap: _showSummaryOptions),
+            _buildListTile('Default Currency', 'USD (\$)', Icons.monetization_on,
+                onTap: _showCurrencyOptions),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildSectionHeader(String title) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Text(
-        title,
-        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  Widget _buildListTile(String title, String subtitle, IconData icon, {bool isSwitch = false}) {
+  Widget _buildListTile(String title, String subtitle, IconData icon,
+      {required VoidCallback onTap}) {
     return ListTile(
       leading: Icon(icon, color: Colors.teal),
       title: Text(title),
       subtitle: Text(subtitle),
-      trailing: isSwitch
-          ? Switch(
-              value: true,
-              onChanged: (value) {},
-            )
-          : null,
-      onTap: () {
-        // Handle tap actions here if needed
-      },
+      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+      onTap: onTap,
     );
   }
 }
-
-
